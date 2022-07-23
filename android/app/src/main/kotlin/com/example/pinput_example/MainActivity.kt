bc94059b221a1d3efb9dc4294fd47c25659f905b
flutter99 <82260050+flutter99@users.noreply.github.com>
2022-07-23 16:29:46 +0500

    project added

---
+++ android/app/src/main/kotlin/com/example/pinput_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pinput_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/verification_code.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pinput/pinput.dart';
+
+class VerificationCodeScreen extends StatefulWidget {
+  const VerificationCodeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<VerificationCodeScreen> createState() => _VerificationCodeScreenState();
+}
+
+class _VerificationCodeScreenState extends State<VerificationCodeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: height * 0.07,
+              ),
+              Text(
+                'Verification Code',
+                style: GoogleFonts.urbanist(
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                  fontSize: 32.0,
+                ),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text:
+                          'Please enter the verification code that we have sent to your email ',
+                      style: GoogleFonts.urbanist(
+                        fontSize: 14.0,
+                        color: const Color(0xff808d9e),
+                        fontWeight: FontWeight.w400,
+                        height: 1.5,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'abcd@gmail.com ',
+                      style: GoogleFonts.urbanist(
+                        fontSize: 14.0,
+                        color: const Color(0xff005BE0),
+                        fontWeight: FontWeight.w400,
+                        height: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: height * 0.1,
+              ),
+
+              /// pinput package we will use here
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: SizedBox(
+                  width: width,
+                  child: Pinput(
+                    length: 4,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    defaultPinTheme: PinTheme(
+                        height: 60.0,
+                        width: 60.0,
+                        textStyle: GoogleFonts.urbanist(
+                          fontSize: 24.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.black.withOpacity(0.5),
+                            width: 1.0,
+                          ),
+                        ),),
+                    focusedPinTheme: PinTheme(
+                      height: 60.0,
+                      width: 60.0,
+                      textStyle: GoogleFonts.urbanist(
+                        fontSize: 24.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 1.0,
+                        ),
+                      ),),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 16.0,
+              ),
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Resend Button',
+                    style: GoogleFonts.urbanist(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+
+              /// Continue Button
+              const Expanded(child: SizedBox()),
+              InkWell(
+                onTap: (){},
+                borderRadius: BorderRadius.circular(30.0),
+                child: Ink(
+                  height: 55.0,
+                  width: width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.black,
+                  ),
+                  child: Center(
+                    child: Text('Continue',
+                      style: GoogleFonts.urbanist(
+                        fontSize: 15.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(
+                height: 16.0,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
